9451be7b8ad24b18c393f82974c28300260da9da
armaanballagan <114946955+armaanballagan@users.noreply.github.com>
2022-10-24 11:11:27 -0700

    Update validate.js

--- contact_folder/validate.js
+++ contact_folder/validate.js
@@ -2,21 +2,20 @@
     This function checks the Color field to make sure it is 3 characters in length
     and it checks the Quantity to make sure it is numeric and from 1 to 12.
 */
-
-      function validate() {
+function validate() {
 
         /* This portion of the function checks the Email Address to make sure
         it has an @ and a dot somewhere in the address.
 */
-        var eaddr = document.getelementbyid("contact-form").email.value;
-        atSign = eaddr.indexOf("@");
-        dotSign = eaddr.lastIndexOf(".");
+    var eaddr = document.getelementbyid("contact-form").email.value;
+    atSign = eaddr.indexOf("@");
+    dotSign = eaddr.lastIndexOf(".");
 
-        if (atSign < 1 || ( dotSign - atSign < 2 )) {
-           alert("Error in email address format!");
-           
-           return false;
-        }
-        alert("accepted");
-        return( true );
-      }
+    if (atSign < 1 || ( dotSign - atSign < 2 )) {
+       alert("Error in email address format!");
+
+       return false;
+    }
+    alert("accepted");
+    return( true );
+  }
